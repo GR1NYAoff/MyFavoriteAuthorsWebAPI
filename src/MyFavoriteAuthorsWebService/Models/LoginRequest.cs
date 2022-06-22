441e1446f6ca823705c4fe5529524b7556af4400
= <=>
2022-06-23 00:32:03 +0300

    added login request model

---
+++ src/MyFavoriteAuthorsWebService/Models/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFavoriteAuthorsWebService.Models
+{
+    public class LoginRequest
+    {
+        [Required]
+        [MaxLength(20, ErrorMessage = "The name must contain no more than 20 characters")]
+        [MinLength(3, ErrorMessage = "The name must contain at least 3 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must contain at least 6 characters")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
