f475b2b38be448309762051b1a6c3c49ed791ff8
= <=>
2022-06-22 23:58:40 +0300

    added Account model

---
+++ src/MyFavoriteAuthorsWebService/Models/Account.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MyFavoriteAuthorsWebService.Enum;
+
+namespace MyFavoriteAuthorsWebService.Models
+{
+    public class Account
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        [MaxLength(20, ErrorMessage = "The name must contain no more than 20 characters")]
+        [MinLength(3, ErrorMessage = "The name must contain at least 3 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must contain at least 6 characters")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordKey { get; set; } = string.Empty;
+
+        [Required]
+        public Role Role { get; set; }
+
+        public Account()
+        {
+
+        }
+    }
+}
