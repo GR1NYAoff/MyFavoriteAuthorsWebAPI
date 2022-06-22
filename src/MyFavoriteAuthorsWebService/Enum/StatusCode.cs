bb2902eb265ce1734f3a3399b427f61986f93041
= <=>
2022-06-23 00:02:04 +0300

    added enums for roles and status codes

---
+++ src/MyFavoriteAuthorsWebService/Enum/StatusCode.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFavoriteAuthorsWebService.Enum
+{
+    public enum StatusCode
+    {
+        OK = 200,
+        BadRequest = 400,
+        Unauthorized = 401,
+        NotFound = 404,
+        InternalServerError = 500
+    }
+}
