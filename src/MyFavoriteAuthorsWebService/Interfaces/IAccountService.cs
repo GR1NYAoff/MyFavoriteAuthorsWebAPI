b0ff87b9ef30aa5358c8feaf1643417071e8a76e
= <=>
2022-06-23 00:00:32 +0300

    added some abstractions

---
+++ src/MyFavoriteAuthorsWebService/Interfaces/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using MyFavoriteAuthorsWebService.Enum;
+using MyFavoriteAuthorsWebService.Models;
+
+namespace MyFavoriteAuthorsWebService.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<StatusCode> Register(Account user);
+
+        Task<(StatusCode, string)> Login(Account user);
+
+    }
+}
