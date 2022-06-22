5f09f6c2611b737810468c7fcb059c891d9f6b32
= <=>
2022-06-23 00:33:32 +0300

    removed validation attributes

--- src/MyFavoriteAuthorsWebService/Models/Account.cs
+++ src/MyFavoriteAuthorsWebService/Models/Account.cs
@@ -12,12 +12,9 @@ namespace MyFavoriteAuthorsWebService.Models
 
         [Required]
         [Column(TypeName = "nvarchar(100)")]
-        [MaxLength(20, ErrorMessage = "The name must contain no more than 20 characters")]
-        [MinLength(3, ErrorMessage = "The name must contain at least 3 characters")]
         public string Name { get; set; } = string.Empty;
 
         [Required]
-        [MinLength(6, ErrorMessage = "Password must contain at least 6 characters")]
         public string Password { get; set; } = string.Empty;
 
         [Required]
