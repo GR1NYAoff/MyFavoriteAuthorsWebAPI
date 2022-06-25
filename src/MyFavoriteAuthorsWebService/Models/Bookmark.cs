8b07c2b34caf559f19eb63fac8f6600537636c46
= <=>
2022-06-25 23:52:13 +0300

    added bookmark model and linked entities

---
+++ src/MyFavoriteAuthorsWebService/Models/Bookmark.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using ServiceStack.DataAnnotations;
+
+namespace MyFavoriteAuthorsWebService.Models
+{
+    public class Bookmark
+    {
+        [Required]
+        [PrimaryKey, AutoIncrement]
+        public int BookmarkId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string AuthorName { get; set; } = string.Empty;
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string AuthorKey { get; set; } = string.Empty;
+        public string Comment { get; set; } = string.Empty;
+
+        public Guid AccountId { get; set; }
+        public Account? Account { get; set; }
+    }
+}
