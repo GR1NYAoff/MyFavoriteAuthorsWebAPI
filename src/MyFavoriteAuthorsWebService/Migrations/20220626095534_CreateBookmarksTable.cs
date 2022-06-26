0b8e78d3d671b7467522e051fc2a8e215e0537d9
= <=>
2022-06-26 21:02:26 +0300

    added migration CreateBookmarksTable

---
+++ src/MyFavoriteAuthorsWebService/Migrations/20220626095534_CreateBookmarksTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFavoriteAuthorsWebService.Migrations
+{
+    public partial class CreateBookmarksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookmarks",
+                columns: table => new
+                {
+                    BookmarkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuthorName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    AuthorKey = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookmarks", x => x.BookmarkId);
+                    table.ForeignKey(
+                        name: "FK_Bookmarks_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookmarks_AccountId",
+                table: "Bookmarks",
+                column: "AccountId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookmarks");
+        }
+    }
+}
