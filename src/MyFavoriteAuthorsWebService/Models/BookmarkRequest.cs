252d5090dede41f07b454c90f125809c22abd47b
= <=>
2022-06-26 21:12:07 +0300

    added bookmark request model

---
+++ src/MyFavoriteAuthorsWebService/Models/BookmarkRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFavoriteAuthorsWebService.Models
+{
+    public class BookmarkRequest
+    {
+        [Required]
+        public string AuthorName { get; set; } = string.Empty;
+        [Required]
+        public string AuthorKey { get; set; } = string.Empty;
+        public string Comment { get; set; } = string.Empty;
+    }
+}
