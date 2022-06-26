aecf20a97d091c4f14353341220d857049ab55ea
= <=>
2022-06-26 21:15:15 +0300

    added controller for Bookmarks

---
+++ src/MyFavoriteAuthorsWebService/Controllers/BookmarkController.cs
@@ -0,0 +1,88 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyFavoriteAuthorsWebService.Interfaces;
+using MyFavoriteAuthorsWebService.Models;
+
+namespace MyFavoriteAuthorsWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BookmarkController : ControllerBase
+    {
+        private readonly IBookmarkService _bookmarkService;
+        private string UserId => User.Claims
+            .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+
+        public BookmarkController(IBookmarkService bookmarkService)
+        {
+            _bookmarkService = bookmarkService;
+        }
+
+        // GET: api/<BookmarkController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bookmark>>> GetAvailableTests()
+        {
+            if (!Guid.TryParse(UserId, out var userId))
+                return Unauthorized();
+
+            return Ok(await _bookmarkService.GetAvailableBookmarks(userId));
+
+        }
+
+        // GET api/<BookmarkController>/1
+        [HttpGet("{bookmarkId}")]
+        public async Task<ActionResult<Bookmark>> GetBookmark(int bookmarkId)
+        {
+            if (!Guid.TryParse(UserId, out var userId))
+                return Unauthorized();
+
+            var bookmark = await _bookmarkService.GetBookmark(bookmarkId, userId);
+
+            return bookmark == null ? NotFound() : Ok(bookmark);
+        }
+
+        // POST api/<BookmarkController>
+        [HttpPost]
+        public async Task<ActionResult> PostBikeDetail([FromBody] BookmarkRequest request)
+        {
+            if (!Guid.TryParse(UserId, out var userId))
+                return Unauthorized();
+
+            var newBookmark = new Bookmark(request.AuthorName, request.AuthorKey,
+                                           request.Comment, userId);
+
+            if (await _bookmarkService.AddBookmark(newBookmark) != Enum.StatusCode.OK)
+                return BadRequest();
+
+            return Ok();
+        }
+
+        // PUT api/<BookmarkController>/2
+        [HttpPut("{bookmarkId}")]
+        public async Task<IActionResult> PutBikeDetail(int bookmarkId, [FromBody] string comment)
+        {
+            if (!Guid.TryParse(UserId, out var userId))
+                return Unauthorized();
+
+            if (await _bookmarkService.UpdateBookmark(comment, bookmarkId, userId) != Enum.StatusCode.OK)
+                return BadRequest();
+
+            return Ok();
+        }
+
+        // DELETE api/<BookmarkController>/3
+        [HttpDelete("{bookmarkId}")]
+        public async Task<IActionResult> DeleteBikeDetail(int bookmarkId)
+        {
+            if (!Guid.TryParse(UserId, out var userId))
+                return Unauthorized();
+
+            if (await _bookmarkService.RemoveBookmark(bookmarkId, userId) != Enum.StatusCode.OK)
+                return BadRequest();
+
+            return Ok();
+        }
+    }
+}
