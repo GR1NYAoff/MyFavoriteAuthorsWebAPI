7af34d18547062b33ad3b3a9f3e4d2771bdc5bb8
= <=>
2022-06-26 23:20:09 +0300

    added Author Controller

---
+++ src/MyFavoriteAuthorsWebService/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFavoriteAuthorsWebService.Interfaces;
+using MyFavoriteAuthorsWebService.Models;
+
+namespace MyFavoriteAuthorsWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet("{authorName}")]
+        public async Task<ActionResult<IEnumerable<BookmarkRequest>>> GetValidAuthorsList(string authorName)
+        {
+            return Ok(await _authorService.GetValidBookmarksRequests(authorName));
+        }
+
+    }
+}
