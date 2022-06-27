3bfc84de5fa2040c7dfa33bc7a4b7592260c79e9
= <=>
2022-06-27 20:41:50 +0300

    added Json formatting to plain text

--- src/MyFavoriteAuthorsWebService/Program.cs
+++ src/MyFavoriteAuthorsWebService/Program.cs
@@ -15,6 +15,20 @@ using Swashbuckle.AspNetCore.Filters;
 const string myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMvc(options =>
+{
+    options.AllowEmptyInputInBodyModelBinding = true;
+    foreach (var formatter in options.InputFormatters)
+    {
+        if (formatter.GetType() == typeof(SystemTextJsonInputFormatter))
+            ((SystemTextJsonInputFormatter)formatter).SupportedMediaTypes.Add(
+            Microsoft.Net.Http.Headers.MediaTypeHeaderValue.Parse("text/plain"));
+    }
+}).AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
